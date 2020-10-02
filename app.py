13dfd994e10b7df6f3b3b711d9b4f87a20fde336
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2020-10-02 16:59:32 -0600

    Hello Flask

--- app.py
+++ app.py
@@ -1,22 +1,15 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask
+from flask import render_template
 import os
 import sys
 
-app = Flask(__name__)
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    
-    # myPlatform = sys.platform
-    
-    return render_template("home.html")
+    return "Hello, Flask!"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
